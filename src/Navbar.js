e0a7cc10aa70ea61188b73a2cfa9cd2dc49b0a03
EzequielCaste <ezequiel.caste@gmail.com>
2020-11-13 11:11:03 -0300

    Responsive Navbar and Form with Material UI

---
+++ src/Navbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Switch from "@material-ui/core/Switch";
+
+import SearchIcon from "@material-ui/icons/Search";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles/NavBarStyles";
+
+class Navbar extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit">
+              <span>
+                <i
+                  className="em em-flag-ar"                 
+                />
+              </span>
+            </IconButton>
+            <Typography className={classes.title} variant="h6" colors="inherit">
+              App title
+            </Typography>
+            <Switch />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Navbar);
