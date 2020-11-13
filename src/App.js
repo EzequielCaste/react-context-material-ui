7be35f11b0af0600be421f93066a487139fd1619
EzequielCaste <ezequiel.caste@gmail.com>
2020-11-13 17:02:13 -0300

    Create the Language Context

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Form from "./Form";
 import Navbar from "./Navbar";
 import PageContent from "./PageContent";
 import {ThemeProvider} from './contexts/ThemeContext'
+import {LanguageProvider} from './contexts/LanguageContext'
 
 class App extends Component {
   render() {
     return (
+      <LanguageProvider>
       <ThemeProvider>
       <PageContent>
         <Navbar />
         <Form />
       </PageContent>
       </ThemeProvider>
+      </LanguageProvider>
     );
   }
 }
